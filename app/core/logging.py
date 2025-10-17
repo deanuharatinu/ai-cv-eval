f3743bfa60c48889faa163a67f9ced38b282cfeb
deanuharatinu <deanu.alt@gmail.com>
2025-10-18 00:06:23 +0700

    chore: reimplement existing loggers for dev debugging

--- app/core/logging.py
+++ app/core/logging.py
@@ -10,6 +10,7 @@ def configure_logging() -> None:
     dictConfig(
         {
             "version": 1,
+            "disable_existing_loggers": False,
             "formatters": {
                 "default": {
                     "format": "%(asctime)s | %(levelname)s | %(name)s | %(message)s",
